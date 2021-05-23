896ef266c82803808705a5a05d89cc87cfa1a9a2
cbatra9 <chandrikabatra2@gmail.com>
2021-05-23 23:11:04 +0530

    Updates

---
+++ precache-manifest.9410c5c05a71a9c9871ad2490919e3d8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aaa76dca17e392c281210685319d0485",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "af1a3913179b5c7c2650",
+    "url": "/home/static/css/main.f5497eb8.chunk.css"
+  },
+  {
+    "revision": "88448ffcfce5fdaf8e09",
+    "url": "/home/static/js/2.cfafeff4.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/home/static/js/2.cfafeff4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af1a3913179b5c7c2650",
+    "url": "/home/static/js/main.9e4c0c94.chunk.js"
+  },
+  {
+    "revision": "c0578b487099793a068c",
+    "url": "/home/static/js/runtime-main.3c590061.js"
+  },
+  {
+    "revision": "459da2842a0588cdb1e8739da4585f02",
+    "url": "/home/static/media/Chandrika Batra.459da284.JPG"
+  },
+  {
+    "revision": "6367a2d73e58ce8c33ec9907d495e57e",
+    "url": "/home/static/media/resume.6367a2d7.pdf"
+  }
+]);
